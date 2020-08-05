1f30fea442842e1c2fa55aaa11e2eb34db09259f
Tom Mitchell <n1527044>
2020-08-05 20:23:18 +0000

    Make dump flags required by cli

--- main.go
+++ main.go
@@ -10,11 +10,11 @@ import (
 func main() {
 	dumpCom := app.Command("dump", "Dump the autoscaling information out of the PCF server")
 	cmdIndex["dump"] = &dumpCmd{
-		ClientID:     dumpCom.Flag("client-id", "The client id to auth with").String(),
-		ClientSecret: dumpCom.Flag("client-secret", "The client secret to auth with").String(),
-		CFHost:       dumpCom.Flag("cf-host", "The CF API host to scrape from").String(),
-		PCFASHost:    dumpCom.Flag("pcfas-host", "The PCF Autoscaler API to talk to").String(),
-		BrokerGUID:   dumpCom.Flag("broker-guid", "The GUID of the autoscaler service broker").String(),
+		ClientID:     dumpCom.Flag("client-id", "The client id to auth with").Required().String(),
+		ClientSecret: dumpCom.Flag("client-secret", "The client secret to auth with").Required().String(),
+		CFHost:       dumpCom.Flag("cf-host", "The CF API host to scrape from").Required().String(),
+		PCFASHost:    dumpCom.Flag("pcfas-host", "The PCF Autoscaler API to talk to").Required().String(),
+		BrokerGUID:   dumpCom.Flag("broker-guid", "The GUID of the autoscaler service broker").Required().String(),
 	}
 
 	convertCom := app.Command("convert", "Display OCF autoscaler converted rules")
